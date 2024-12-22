c2ad33f4b75c4034289b742c98214d9286168f16
Bram Borggreve <beeman@beeman.nl>
2024-12-22 00:33:19 +0000

    feat: replace Tailwind with Mantine

--- app/root.tsx
+++ app/root.tsx
@@ -1,20 +1,14 @@
+import '@mantine/core/styles.css'
+
+import { ColorSchemeScript, MantineProvider } from '@mantine/core'
 import type { LinksFunction, LoaderFunctionArgs } from "@remix-run/cloudflare";
 import { Links, Meta, Outlet, Scripts, ScrollRestoration, useLoaderData, } from "@remix-run/react";
-
-import "./tailwind.css";
 import { AppConfig, AppConfigProvider } from "~/app-config";
 
+import stylesheet from './app.css?url'
+
 export const links: LinksFunction = () => [
-  {rel: "preconnect", href: "https://fonts.googleapis.com"},
-  {
-    rel: "preconnect",
-    href: "https://fonts.gstatic.com",
-    crossOrigin: "anonymous",
-  },
-  {
-    rel: "stylesheet",
-    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
-  },
+  {rel: 'stylesheet', href: stylesheet},
 ];
 
 export function loader({context}: LoaderFunctionArgs) {
@@ -37,10 +31,13 @@ export function Layout({children}: { children: React.ReactNode }) {
       <meta name="viewport" content="width=device-width, initial-scale=1"/>
       <Meta/>
       <Links/>
+      <ColorSchemeScript defaultColorScheme="dark"/>
     </head>
     <body>
     <AppConfigProvider config={config}>
-      {children}
+      <MantineProvider defaultColorScheme="dark">
+        {children}
+      </MantineProvider>
     </AppConfigProvider>
     <ScrollRestoration/>
     <Scripts/>
